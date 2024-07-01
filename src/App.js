7a03775c511b3ba694f9f1180e08005be797eb7d
Anirban Kapil <anirbankapil@gmail.com>
2024-07-01 20:42:49 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Body from "./components/Body";
 
 
 function App() {
   return (
    <>
-   <h1 className="bg-black text-white font-semibold">Hello Netflix</h1>
+   <Body />  
    </>
   );
 }
