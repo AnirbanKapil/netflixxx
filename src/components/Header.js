5d4b73155b5b6a2bd096c1d8706f9fa5ace11de5
Anirban Kapil <anirbankapil@gmail.com>
2024-07-01 22:10:42 +0530

    setting up components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='absolute z-10 px-16 py-6 bg-gradient-to-b from-black'>
+        <img
+         className='w-40'
+         alt='icon' 
+         src='https://myflix-platform.vercel.app/assets/myflix-logo.png' />
+    </div>
+  )
+}
+
+export default Header
