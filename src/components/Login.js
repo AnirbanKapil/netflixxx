4ea03898a28362c05ea06eb9eafb281ede9d5068
Anirban Kapil <anirbankapil@gmail.com>
2024-07-02 12:33:25 +0530

    email password validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,14 +5,18 @@ import { checkValidateData } from '../utils/validate'
 function Login() {
   
   const [isSignInForm,setIsSignInForm] = useState(true)
+  const [errorMsg,setErrorMsg] = useState(null);
+
 
   const email = useRef(null);
   const password = useRef(null);
 
   const handleButtonClick = () => {
-    // checkValidateData(email,password)
-    console.log(email.current.value,password.current.value)
+    
+    const msg = checkValidateData(email.current.value,password.current.value)
+    setErrorMsg(msg)
   }
+  
 
 
   const toggleSignInForm = () => {
@@ -27,7 +31,7 @@ function Login() {
       <img className='w-full h-screen' alt='backGroundImg' src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f562aaf4-5dbb-4603-a32b-6ef6c2230136/dh0w8qv-9d8ee6b2-b41a-4681-ab9b-8a227560dc75.jpg/v1/fill/w_1192,h_670,q_70,strp/the_netflix_login_background__canada__2024___by_logofeveryt_dh0w8qv-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZjU2MmFhZjQtNWRiYi00NjAzLWEzMmItNmVmNmMyMjMwMTM2XC9kaDB3OHF2LTlkOGVlNmIyLWI0MWEtNDY4MS1hYjliLThhMjI3NTYwZGM3NS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.LOYKSxIDqfPwWHR0SSJ-ugGQ6bECF0yO6Cmc0F26CQs'/>
       </div>
       <div className='bg-black'>
-        <form className='absolute w-3/12 mt-56 mx-auto right-0 left-0 text-white bg-opacity-60 bg-black rounded-lg' onSubmit={(e)=> e.preventDefault()}> 
+        <form className='absolute w-3/12 mt-56 mx-auto right-0 left-0 text-white bg-opacity-80 bg-black rounded-lg' onSubmit={(e)=> e.preventDefault()}> 
           <h1 className='font-bold text-3xl py-4 m-3'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
           {!isSignInForm && <input type='text' placeholder='Full Name' 
           className='p-2 m-2 w-[300px] bg-gray-700'/>}
@@ -39,6 +43,7 @@ function Login() {
           ref={password} 
           type='password' placeholder='Password' 
           className='p-2 m-2 w-[300px] bg-gray-700'/>
+          <p className='m-2 text-red-800 font-semibold'>{`${"* "}${errorMsg}`}</p>
           <button className='p-3 m-2 mb-6 bg-red-700 text-white rounded-lg w-[300px]' 
           onClick={handleButtonClick}>
             {isSignInForm ? "Sign In" : "Sign Up"}</button>
