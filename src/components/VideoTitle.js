3f1ffe0e469cb4f013ac205dba2dd96d98a6abbb
Anirban Kapil <anirbankapil@gmail.com>
2024-07-10 11:56:36 +0530

    background video setup

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function VideoTitle({title,overview}) {
   return (
-    <div className='pt-36 mx-6'>
+    <div className='w-screen aspect-video pt-52 pl-6 absolute text-white bg-gradient-to-r from-black opacity-100'>
       <h1 className='font-extrabold text-6xl'>{title}</h1>
-      <p className='pt-6 text-lg w-1/2'>{overview}</p>
+      <p className='pt-6 text-sm w-1/4'>{overview}</p>
       <div>
-        <button className='px-6 py-2 m-2 bg-gray-400 rounded-xl'>▶️ Play</button>
-        <button className='px-6 py-2 m-2 bg-gray-400 rounded-xl'>More Info</button>
+        <button className='px-6 py-2 m-2 bg-gray-800 rounded-xl hover:bg-opacity-60'>▶️ Play</button>
+        <button className='px-6 py-2 m-2 bg-slate-800 rounded-xl hover:bg-opacity-60'>ℹ More Info</button>
       </div>
     </div>
   )
